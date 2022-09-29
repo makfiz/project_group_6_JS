4a00829a98b5e991e7ea3858398562176a271d1b
Aleks Haran <nporock@gmail.com>
2022-09-29 17:15:56 +0300

    add-btn-to-top

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-
 import './js/apiServise';
 import './js/refs';
 import './js/searchFilms';
 import './js/templates/renderMovieGallary';
 
-
 // import './js/myLibrary';
 
 import './js/authorization.js';
 
-import './js/gengers.js'
+import './js/gengers.js';
 
+import './js/btn-to-top';
