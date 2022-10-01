0bf0b78e0562cbf4e777c6d52e433af00b2e910d
Illia <2201illia@gmail.com>
2022-10-01 03:10:04 +0300

    fixed nav btns

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -13,16 +13,19 @@ const {
 
 function openLibrary() {
   gallery.innerHTML = '';
+  homeBtn.addEventListener('click', openHome);
   search.classList.add('visually-hidden');
   libraryFilter.classList.remove('visually-hidden');
-  homeBtn.addEventListener('click', openHome);
-
+  homeBtn.classList.remove('nav-list__link--selected');
+  libraryBtn.classList.add('nav-list__link--selected');
   pagEl.classList.add('visually-hidden');
   filterContainer.classList.add('visually-hidden');
 }
 
 function openHome() {
   location.reload();
+  // homeBtn.classList.add('nav-list__link--selected');
+  // libraryBtn.classList.remove('nav-list__link--selected');
   // gallery.innerHTML = '';
   // search.classList.remove('visually-hidden');
   // libraryFilter.classList.add('visually-hidden');
