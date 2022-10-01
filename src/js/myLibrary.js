5d54c68d6d891d5ce479cdfd656c9eb97fc8d5a7
Maksym Hlanskov <makfiz@gmail.com>
2022-10-01 17:40:42 +0300

    add FB

--- src/js/myLibrary.js
+++ src/js/myLibrary.js
@@ -6,45 +6,50 @@ import { pagination } from './pagination';
 import { FirebaseService } from "./firebaseservice";
 import { ApiServise } from './apiServise';
 export const apiServise = new ApiServise();
-
+import { emailCuter } from './authorization';
 const firebase = new FirebaseService();
 
 // function openLibrary() {
-//   // if
- 
+//
+//  -------------------------------> Merget to navigation <----------------------
 
 //   search.classList.add('visually-hidden');
 //   libraryFilter.classList.remove('visually-hidden');
 //   galleryLibrary.classList.remove('visually-hidden');
 //   galleryLibrary.classList.add('library');
 // }
-
-function loadWatced() {
-  watchedBtn.classList.add('library__btn--selected');
-  queueBtn.classList.remove('library__btn--selected');
-  galleryMain.innerHTML = '';
-  //TODO: додати рендер карток при натискані на кнопку
-}
-function loadQueue() {
-  watchedBtn.classList.remove('library__btn--selected');
-  queueBtn.classList.add('library__btn--selected');
-  galleryMain.innerHTML = '';
-  //TODO: додати рендер карток при натискані на кнопку
-}
-
-watchedBtn.addEventListener('click', loadWatced);
-queueBtn.addEventListener('click', loadQueue);
+export async function loadWatced(user) {
+    refs.watchedBtn.classList.add('library__btn--selected');
+    refs.queueBtn.classList.remove('library__btn--selected');
+    galleryLibrary.innerHTML = '';
+    const data = await firebase.GetUserWached(emailCuter(user.email))
+    //TODO: додати рендер карток при натискані на кнопку
+  }
+      
+export  async function loadQueue(user) {
+    refs.watchedBtn.classList.remove('library__btn--selected');
+    refs.queueBtn.classList.add('library__btn--selected');
+    galleryLibrary.innerHTML = '';
+      const data = await firebase.GetUserQueue(emailCuter(user.email))
+    //TODO: додати рендер карток при натискані на кнопку
+  }
+//  -------------------------------> addEventListener loadWatced AND loadQueue Merget to authorization
+// watchedBtn.addEventListener('click', loadWatced);
+// queueBtn.addEventListener('click', loadQueue); <----------------------
 
 // libraryBtn.addEventListener('click', () => {
 //   openLibrary()
 //   onTrendMovies()
 // });
 
-export async function onTrendMovies() {
-  const res = await apiServise.fetchTrendingMovies();
 
-  makeGallaryLibrary(res.results);
-  apiServise.totalPage = res.total_pages;
-  pagination(apiServise);
-}
+
+  //  -------------------------------> test <----------------------
+
+// export async function onTrendMovies() {
+//   const res = await apiServise.fetchTrendingMovies();
+//   makeGallaryLibrary(res.results);
+//   apiServise.totalPage = res.total_pages;
+//   pagination(apiServise);
+// }
 // libraryBtn.addEventListener('click', onTrendMovies);
