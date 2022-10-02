d1b37c0d8fd90abf876ddd4a94b373cfe08859cb
Illia <2201illia@gmail.com>
2022-10-03 01:09:47 +0300

    Update renderMoviesLibrary.js

--- src/js/templates/renderMoviesLibrary.js
+++ src/js/templates/renderMoviesLibrary.js
@@ -1,7 +1,6 @@
 import genresData from '../../js/genresData.json';
 import { refs } from '../refs';
 
-
 export function makeGallaryLibrary(movies) {
   // console.log(movies)
 
@@ -9,34 +8,34 @@ export function makeGallaryLibrary(movies) {
     refs.galleryLibrary.innerHTML = `<span style=" margin-left: auto;
        margin-right: auto;font-size: 30px;
     line-height: 1.19; color: #ff6b08;">Your library is empty ;(</span>`;
-    
-    return
+
+    return;
   }
 
   let temp = [];
   for (let key in movies) {
     // console.log(movies[key])
-    const { id, poster_path, title, release_date, ganre, vote_average } = movies[key]
-    
+    const { id, poster_path, title, release_date, ganre, vote_average } =
+      movies[key];
 
-      let releaseDate = release_date;
+    let releaseDate = release_date;
 
-      if (releaseDate === undefined) {
-        releaseDate = '2022';
-      } else {
-        releaseDate = release_date.slice(0, 4);
-      }
-     
-    let filmsGanres = ganre.replace(/,/g, ', ')
-   
-      let image = `https://image.tmdb.org/t/p/w400/${poster_path}`;
-      if (poster_path === null) {
-        image =
-          'https://dummyimage.com/400x600/7d7d7d/fff.jpg&text=image+not+found';
-      }
+    if (releaseDate === undefined) {
+      releaseDate = '2022';
+    } else {
+      releaseDate = release_date.slice(0, 4);
+    }
 
-      temp.push(
-       ` <li class="gallery__item" data-id = ${id}>
+    let filmsGanres = ganre.replace(/,/g, ', ');
+
+    let image = `https://image.tmdb.org/t/p/w400/${poster_path}`;
+    if (poster_path === null) {
+      image =
+        'https://dummyimage.com/400x600/7d7d7d/fff.jpg&text=image+not+found';
+    }
+
+    temp.push(
+      ` <li class="gallery__item" data-id = ${id}>
             <div class="gallery-card">
               <div class="gallery-card__main">
                 <img
@@ -59,8 +58,8 @@ export function makeGallaryLibrary(movies) {
                 </p>
               </div>
             </div>
-          </li>`)
-  
-  } 
-  refs.galleryLibrary.innerHTML = temp.join();
+          </li>`
+    );
+  }
+  refs.galleryLibrary.innerHTML = temp.join('');
 }
